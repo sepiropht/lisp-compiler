176d623e7a2961fb8016bf2bddf4a84181102cbe
sepiropht <sergembotta@mailoo.org>
2019-08-17 00:43:10 +0200

    error handling

--- src/main.c
+++ src/main.c
@@ -23,6 +23,51 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+typedef struct {
+  int type;
+  long num;
+  int err;
+} lval;
+
+enum { LVAL_NUM, LVAL_ERR };
+
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+lval lval_num(long x) {
+  lval v;
+  v.type = LVAL_NUM;
+  v.num = x;
+  return v;
+}
+
+lval lval_err(int x) {
+  lval v;
+  v.type = LVAL_ERR;
+  v.err = x;
+  return v;
+}
+
+void lval_print(lval v) {
+  switch (v.type) {
+    case LVAL_NUM: printf("%li", v.num); break;
+
+    case LVAL_ERR:
+	if(v.err == LERR_DIV_ZERO) {
+		printf("Error: Division By Zero!");
+	}
+
+	if(v.err == LERR_BAD_OP) {
+	  printf("Error: Invalid operator!");
+	}
+	if(v.err == LERR_BAD_NUM) {
+	  printf("Error: Invalid Number!");
+	}
+    break;
+  }
+}
+
+void lval_println(lval v) { lval_print(v); putchar('\n'); }
+
 int number_of_nodes(mpc_ast_t* t) {
   if(t->children_num == 0) {  return 1;}
   if(t->children_num >= 1) {
@@ -35,24 +80,32 @@ int number_of_nodes(mpc_ast_t* t) {
   return 0;
 }
 
-long eval_op(long x, char* op, long y) {
-   if(strcmp(op, "+") == 0) { return x + y; }
-   if(strcmp(op, "-") == 0) { return x - y; }
-   if(strcmp(op, "*") == 0) { return x * y; }
-   if(strcmp(op, "/") == 0)  { return x / y; }
+lval eval_op(lval x, char* op, lval y) {
+   if(x.type == LVAL_ERR) { return x; }
+   if(y.type == LVAL_ERR) { return y; }
+   if(strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
+   if(strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
+   if(strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
+   if(strcmp(op, "/") == 0)  { 
+	   return y.num == 0 
+           ? lval_err(LERR_DIV_ZERO)
+	   : lval_num(x.num / y.num); 
+   }
 
-   return 0;
+   return lval_err(LERR_BAD_OP);
 
 }
 
-long eval(mpc_ast_t* t) {
+lval eval(mpc_ast_t* t) {
 	if(strstr(t->tag, "number")) {
-		return atoi(t->contents);
+		errno = 0;
+		long x = strtol(t->contents, NULL, 10);
+		return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
 	}
 
 	char* op = t->children[1]->contents;
 
-	long x = eval(t->children[2]);
+	lval x = eval(t->children[2]);
 
 	int i = 3;
 
@@ -94,8 +147,8 @@ int main(int argc, char** argv) {
 		mpc_result_t r;
 
 		if(mpc_parse("<stdin>", input, Lispy, &r)) {
-		  long result = eval(r.output);
-		  printf("%li\n", result);
+		  lval result = eval(r.output);
+		  lval_println(result);
 		  mpc_ast_delete(r.output);
 		} else {
 		  mpc_err_print(r.error);
