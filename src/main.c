8390a4da0208aa4272aa497c248f2767e3a61c23
sepiropht <sergembotta@mailoo.org>
2019-08-30 11:54:35 +0200

    eval join Q-expression

--- src/main.c
+++ src/main.c
@@ -223,6 +223,55 @@ void lval_print(lval* v) {
   }
 }
 
+lval* lval_join(lval* x, lval* y) {
+
+  /* For each cell in 'y' add it to 'x' */
+  while (y->count) {
+    x = lval_add(x, lval_pop(y, 0));
+  }
+
+  /* Delete the empty 'y' and return 'x' */
+  lval_del(y);
+  return x;
+}
+
+lval* builtin_join(lval* a) {
+
+  for (int i = 0; i < a->count; i++) {
+    LASSERT(a, a->cell[i]->type == LVAL_QEXPR,
+      "Function 'join' passed incorrect type.");
+  }
+
+  lval* x = lval_pop(a, 0);
+
+  while (a->count) {
+    x = lval_join(x, lval_pop(a, 0));
+  }
+
+  lval_del(a);
+  return x;
+}
+
+lval* builtin_list(lval* a) {
+  a->type = LVAL_QEXPR;
+  return a;
+}
+
+lval* lval_eval(lval* v);
+
+lval* builtin_eval(lval* a) {
+  LASSERT(a, a->count == 1,
+    "Function 'eval' passed too many arguments!");
+  LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+    "Function 'eval' passed incorrect type!");
+
+  lval* x = lval_take(a, 0);
+  x->type = LVAL_SEXPR;
+  return lval_eval(x);
+}
+
+
+
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
 int number_of_nodes(mpc_ast_t* t) {
@@ -271,7 +320,6 @@ lval* builtin_op(lval* a, char* op) {
       }
       x->num /= y->num;
     }
-
     lval_del(y);
   }
 
@@ -279,14 +327,17 @@ lval* builtin_op(lval* a, char* op) {
 }
 
 lval* builtin(lval* a, char* func) {
+  if (strcmp("list", func) == 0) { return builtin_list(a); }
   if (strcmp("head", func) == 0) { return builtin_head(a); }
   if (strcmp("tail", func) == 0) { return builtin_tail(a); }
+  if (strcmp("join", func) == 0) { return builtin_join(a); }
+  if (strcmp("eval", func) == 0) { return builtin_eval(a); }
   if (strstr("+-/*", func)) { return builtin_op(a, func); }
   lval_del(a);
   return lval_err("Unknown Function!");
 }
 
-lval* lval_eval(lval* v);
+
 
 lval* lval_eval_sexpr(lval* v) {
 	for(int i = 0; i < v->count; i++) {
@@ -335,7 +386,7 @@ mpca_lang(MPCA_LANG_DEFAULT,
   "                                                        \
     number : /[+-]?([0-9]*[.])?[0-9]+/ ;                                  \
     symbol : \"list\" | \"head\" | \"tail\"                \
-           | \"join\" | \"eval\" | '+' | '-' | '*' | '/' ; \
+           | \"join\" | \"eval\" | '+' | '-' | '*' | '/' | '%' ; \
     sexpr  : '(' <expr>* ')' ;                             \
     qexpr  : '{' <expr>* '}' ;                             \
     expr   : <number> | <symbol> | <sexpr> | <qexpr> ;     \
